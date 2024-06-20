20dfd2c99d0bb904e585d1aadf8347f27a7d89e9
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-06-21 00:03:33 +0530

    sign-up ui done

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,28 +1,37 @@
 "use client";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import React, { use, useEffect, useState } from "react";
 
-const LoginPage = () => {
+const SignUp = () => {
   const [disable, setDisable] = useState(true);
   const [user, setUser] = useState({
+    username: "",
     email: "",
     password: "",
   });
 
   useEffect(() => {
-    if (user.email && user.password) {
+    if (user.email && user.password && user.username) {
       setDisable(false);
-    }else{
-      setDisable(true)
+    } else {
+      setDisable(true);
     }
   }, [user]);
-
   return (
     <div className="flex bg-[#669bbc] min-h-screen justify-center items-center">
       <div className="bg-white p-8 rounded-md shadow-lg shadow-black">
         <h1 className=" flex text-2xl justify-center items-center font-bold">
-          Login
+          Sign up
         </h1>
+        <div className="flex flex-col my-4">
+          <label className="font-bold"> Username</label>
+          <input
+            type="text"
+            className="border-2 outline-none border-zinc-600 rounded-md px-2 py-1"
+            value={user.username}
+            onChange={(e) => setUser({ ...user, username: e.target.value })}
+          />
+        </div>
         <div className="flex flex-col my-4">
           <label className="font-bold"> Email</label>
           <input
@@ -41,17 +50,13 @@ const LoginPage = () => {
             onChange={(e) => setUser({ ...user, password: e.target.value })}
           />
         </div>
-        <button
-          className={`${
-            disable ? "bg-[#e3e3e3] cursor-not-allowed" : "bg-[#ff6984]"
-          } w-full py-1 my-2 rounded-md text-white`}
-        >
-          Login
+        <button className={`${disable ? "bg-[#e3e3e3] cursor-not-allowed": "bg-[#ff6984]"} w-full py-1 my-2 rounded-md text-white`}>
+          Signup
         </button>
         <p className="mt-4">
-          Dont have an account?{" "}
-          <Link href={"/sign-up"} className="font-bold">
-            Signup{" "}
+          Already have an account?{" "}
+          <Link href={"/login"} className="font-bold">
+            Login{" "}
           </Link>
         </p>
       </div>
@@ -59,4 +64,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default SignUp;
